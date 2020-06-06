a17d585d801f5c7b22f6badeb78d93819f713c1d
David Bellers <davidbellers@comentec.com>
2020-06-06 16:20:35 -0400

    layout

---
+++ layouts/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-content class="teal lighten-5">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dark:true,
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Doricus"
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
